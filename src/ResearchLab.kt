c862e6a99a8dc95fc39c2442e8e805a81ae35c84
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-01-20 21:50:31 +0300

    Add abstract modules, interfaces andfirst moduleimplementations

---
+++ src/ResearchLab.kt
@@ -0,0 +1,13 @@
+class ResearchLab: OutpostModule("Исследовательская лаборатория") {
+    override fun performAction(manager: ResourceManager) {
+        val minerals = manager.get("Minerals")
+        if (minerals == null || minerals.amount < 30){
+            println("Недостаточно минералов для исследования")
+            return
+        }
+        else{
+            minerals.amount -= 30
+            println("Лаборатория производит исследование (минералы -30)")
+        }
+    }
+}
