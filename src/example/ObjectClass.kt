8ff4f229ae376a11abe5c8112b15c96e38744446
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-02-01 16:40:35 +0300

    learn sealed-class

--- src/example/ObjectClass.kt
+++ src/example/ObjectClass.kt
@@ -1,2 +1,90 @@
 package example
 
+object GameSession{
+    init{
+        println("Игровая сессия создана")
+    }
+    var isActive: Boolean = false
+    fun start(){
+        isActive = true
+        println("Игра начилась")
+    }
+    fun end(){
+        isActive = false
+        println("Игра завершена")
+    }
+}
+//fun main(){
+//    println("Программа запущена")
+//    println("Проверяем состояние, но не трогаем GameSession")
+//    println("Теперь запускаем игру")
+//    GameSession.start()
+//    println("Проверяем состояние еще раз")
+//    println("Активнаи ли сессия: ${GameSession.isActive}")
+//}
+
+object Logger{
+    var count = 0
+    fun log(message: String){
+        count++
+        println("[$count] $message")
+    }
+}
+
+//fun main(){
+//    Logger.log("First message")
+//    Logger.log("Second message")
+//    val logger1 = Logger
+//    val logger2 = Logger
+//    println(logger1 == logger2)
+//}
+
+object AppSetting {
+    val version = "1.0.0"
+    var isDarkMode = true
+    fun toggleTheme(){
+        isDarkMode = !isDarkMode
+    }
+    fun checkTheme(){
+        if(AppSetting.isDarkMode){
+            println("Тёмная тема включена")
+        }
+    }
+}
+
+object Colors{
+    const val RED = "#FF0000"
+    const val GREEN = "#00FF00"
+    const val BLUE = "#0000FF"
+}
+//fun main(){
+//    println(Colors.RED)
+//    println(Colors.GREEN)
+//    println(Colors.BLUE)
+//}
+
+//fun main(){
+//    val handler = object {
+//        val name = "Обработчик"
+//        fun handle(){
+//            println("Обрабатываю...")
+//        }
+//    }
+//    println(handler.name)
+//    handler.handle()
+//}
+
+class MyCar(val model: String){
+    fun drive() = println("$model едет")
+}
+object TrafficController{
+    var carCount = 0
+    fun carPassed(){
+        carCount++
+    }
+}
+fun main(){
+    MyCar("BMW")
+    MyCar("Toyota")
+    TrafficController.carPassed()
+}
--- src/example/SealedClass.kt
+++ src/example/SealedClass.kt
@@ -1,14 +1,21 @@
 package example
 
 import javax.annotation.processing.Messager
+import javax.print.attribute.standard.JobStateReason
 
 sealed class NetworkResult{
     data class Success(val data: String) : NetworkResult()
     data class Error(val message: String, val code: Int) : NetworkResult()
     object Loading : NetworkResult()
 }
+sealed class OrderStatus{
+    object Created: OrderStatus()
+    object Paid: OrderStatus()
+    object Shipped: OrderStatus()
+    data class Cancelled(val reason: String) : OrderStatus()
+}
 
-fun  handleResult(result: NetworkResult){
+fun handleResult(result: NetworkResult){
     when(result){
         is NetworkResult.Success -> {
             println("Успех: ${result.data}")
@@ -21,11 +28,25 @@ fun  handleResult(result: NetworkResult){
         }
     }
 }
+fun handleOrder(status: OrderStatus){
+    when(status){
+        OrderStatus.Created -> println("Заказ создан")
+        OrderStatus.Paid -> println("Заказ оплачен")
+        OrderStatus.Shipped -> println("Заказ отправлен")
+        is OrderStatus.Cancelled -> println("Отменён: ${status.reason}")
+    }
+}
+
 fun main(){
-    val success = NetworkResult.Success("Данные полученные")
-    val error = NetworkResult.Error("Сервер не отвечает", 500)
-    val loading = NetworkResult.Loading
-    handleResult(success)
-    handleResult(error)
-    handleResult(loading)
+//    val success = NetworkResult.Success("Данные полученные")
+//    val error = NetworkResult.Error("Сервер не отвечает", 500)
+//    val loading = NetworkResult.Loading
+//    handleResult(success)
+//    handleResult(error)
+//    handleResult(loading)
+
+    handleOrder(OrderStatus.Created)
+    handleOrder(OrderStatus.Paid)
+    handleOrder(OrderStatus.Shipped)
+    handleOrder(OrderStatus.Cancelled("Нет товара на складе"))
 }
